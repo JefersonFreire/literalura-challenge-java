6ed65dd3b24cc0b5e03a2ccd96637084c14c36e6
JefersonFreire <jfreire86@hotmail.com>
2025-07-05 11:19:47 -0300

    feat: modelando dados

---
+++ src/main/java/br/com/jeferson/literalura/model/Livro.java
@@ -0,0 +1,62 @@
+package br.com.jeferson.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String idioma;
+    private Integer numeroDownloads;
+
+    @ManyToOne
+    private Autor autor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getNumeroDownloads() {
+        return numeroDownloads;
+    }
+
+    public void setNumeroDownloads(Integer numeroDownloads) {
+        this.numeroDownloads = numeroDownloads;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", idioma='" + idioma + '\'' +
+                ", numeroDownloads=" + numeroDownloads +
+                '}';
+    }
+}
