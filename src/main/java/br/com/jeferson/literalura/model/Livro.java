94c7e8f229e63b49cf786542acf2b436f1f56d46
JefersonFreire <jfreire86@hotmail.com>
2025-07-13 11:39:27 -0300

    alterando a strategy de id em livro

--- src/main/java/br/com/jeferson/literalura/model/Livro.java
+++ src/main/java/br/com/jeferson/literalura/model/Livro.java
@@ -9,7 +9,7 @@ import jakarta.persistence.*;
 public class Livro {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String idioma;
